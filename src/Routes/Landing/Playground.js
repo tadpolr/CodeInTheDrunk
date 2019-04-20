3b40e5102d25bd36db928ac8b93eb17c55dd7afa
Tadpol Rachatasumrit <tadpol@skooldio.com>
2019-04-20 17:04:15 +0700

    Fixed merge conflict

--- src/Routes/Landing/Playground.js
+++ src/Routes/Landing/Playground.js
@@ -3,22 +3,22 @@ import Car from '../../Components/Car';
 import Road from '../../Components/Road';
 import Roadtask from '../../Components/Roadtask';
 
-
 const LandingPlayground = () => {
-  return <div>
-     <Car progress={'40'}>
-        User A
-        <img src="/images/carBlue.png" alt="My test image"></img>
-     </Car>
-     <Road>
+  return (
+    <div>
+      <Car progress={'40'}>
+        User A<img src="/images/carBlue.png" alt="My test image" />
+      </Car>
+      <Road>
         Road
         <Roadtask> Task 1 </Roadtask>
         <Roadtask> Task 2 </Roadtask>
         <Roadtask> Task 3 </Roadtask>
         <Roadtask> Task 4 </Roadtask>
         <Roadtask> Task 5 </Roadtask>
-     </Road>
-  </div>;
+      </Road>
+    </div>
+  );
 };
 
 export default LandingPlayground;

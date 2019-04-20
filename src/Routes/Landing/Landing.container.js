2a212888cca287907b20c896add47b8eeab11904
Tadpol Rachatasumrit <tadpol@skooldio.com>
2019-04-20 21:22:56 +0700

    Commented out unused code

--- src/Routes/Landing/Landing.container.js
+++ src/Routes/Landing/Landing.container.js
@@ -6,15 +6,15 @@ const Landing = ({ match }) => {
   const roomId = match.params.roomId;
   const [players, setPlayers] = useState(null);
 
-  useEffect(() => {
-    subscribeRoomData(roomId, snapshot => {
-      const { players } = snapshot.val() || {};
-      const playersArr = Object.keys(players).map(player => {
-        return players[player];
-      });
-      setPlayers(playersArr);
-    });
-  }, []);
+  //   useEffect(() => {
+  //     subscribeRoomData(roomId, snapshot => {
+  //       const { players } = snapshot.val() || {};
+  //       const playersArr = Object.keys(players).map(player => {
+  //         return players[player];
+  //       });
+  //       setPlayers(playersArr);
+  //     });
+  //   }, []);
   return <LandingView players={players} />;
 };
 

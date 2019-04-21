ecf05d85a70b5860152e55e1be0d30b8217c802f
Tadpol Rachatasumrit <tadpol@skooldio.com>
2019-04-21 12:18:05 +0700

    Added new image

--- src/Components/CompleteModal.js
+++ src/Components/CompleteModal.js
@@ -9,7 +9,12 @@ const CompleteModal = ({ onClose, open, name = '' }) => {
         style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', padding: '42px' }}
       >
         <h1 style={{ marginBottom: '32px' }}>Congratulation!</h1>
-        <img width={'150px'} src="/images/beer.png" alt="beer" style={{ marginBottom: '20px' }} />
+        <img
+          width={'300px'}
+          src="/images/congrats.png"
+          alt="beer"
+          style={{ marginBottom: '20px' }}
+        />
         <h2>{name.toUpperCase()} has finished all the tasks.</h2>
       </div>
     </Dialog>
